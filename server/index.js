6bbaf698c8223ce6e5a93f30ec4ff6342d97909e
Joseph Lee <38001452+josephlee222@users.noreply.github.com>
2024-08-30 23:22:59 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const connector = new MyInfoConnector(MYINFO_CONNECTOR_CONFIG);
 var sessionIdCache = {};
 
 const corsOptions = {
-  origin: true,
+  origin: "*",
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
